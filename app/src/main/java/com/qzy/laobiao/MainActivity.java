bba5bc1e55449ef1033b76cdb8e3fe99215cb200
laimiewoa <fycdr@qq.com>
2020-09-21 14:32:01 +0800

    第三次提交

--- app/src/main/java/com/qzy/laobiao/MainActivity.java
+++ app/src/main/java/com/qzy/laobiao/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends BasePresenterFragActivity<HomePresenter> imple
         //获取存储卡权限
         setRxPermissions();
         rxPermissions = new RxPermissions(context);
-        ToastManager.showToast(getApplicationContext(),"sec test");
+        ToastManager.showToast(getApplicationContext(),"啦啦啦啦啦啦啦啦");
     }
 
 
